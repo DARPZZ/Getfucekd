302a7202a950d4d260e862862a8d42a4064c1915
rasmustofth <104558794+DARPZZ@users.noreply.github.com>
2023-04-13 13:26:24 +0200

    okhhhk

--- Devops/src/test/TestIt.java
+++ Devops/src/test/TestIt.java
@@ -1,11 +1,8 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.*;
 public class TestIt
-
 {
     @BeforeEach
 
